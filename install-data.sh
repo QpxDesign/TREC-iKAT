db4a2fc5d2500687bd1e801d64bc224f66b0c4ea
Quinn Patwardhan <71609644+QpxDesign@users.noreply.github.com>
2023-07-15 12:42:28 -0400

    Update install-data.sh

--- install-data.sh
+++ install-data.sh
@@ -1,3 +1,4 @@
 curl -o ./data/fastText/cc.en.300.bin.gz https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
+mkdir ./data/fastText/
 cd ./data/fastText/
 gzip -d cc.en.300.bin.gz 
