18e5ad513b804c31a1ed682b5001dcdeedebcba5
Quinn Patwardhan <quinnpatwardhan@quinns-mbp.lan>
2023-07-19 18:48:21 -0400

    added llama download support

--- install-data.sh
+++ install-data.sh
@@ -1,5 +1,14 @@
-# download fastText pretrained vectors english
-curl -o ./data/fastText/cc.en.300.bin.gz https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
-mkdir ./data/fastText/
-cd ./data/fastText/
-gzip -d cc.en.300.bin.gz 
+# download fastText pretrained vectors english  (~6GB)
+
+#mkdir ./data/fastText/
+#curl -o ./data/fastText/cc.en.300.bin.gz https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
+#cd ./data/fastText/
+#gzip -d cc.en.300.bin.gz 
+#cd ../../
+
+
+# download llama 7B chat (~12GB)
+mkdir ./data/llama/
+curl -o ./data/llama/llama-2-7b-chat.zip https://media.quinnpatwardhan.com/data/llama-2-7b-chat.zip
+cd ./data/llama/zx
+unzip ./data/llama-2-7b-chat.zip -d llama-2-7b-chat
