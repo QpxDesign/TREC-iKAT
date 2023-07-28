6f792d979fe706f1eb7f642f319221eb3cb88a10
qpxdesign <qroshan5@gmail.com>
2023-07-28 13:08:42 -0400

    added clueweb support into get-passages and readme

--- get-passages.py
+++ get-passages.py
@@ -1,15 +1,7 @@
 from pyserini.search.lucene import LuceneSearcher
-from pyserini.search.faiss import FaissSearcher, TctColBertQueryEncoder
-from pyserini.search.hybrid import HybridSearcher
 
-ssearcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
-encoder = TctColBertQueryEncoder('castorini/tct_colbert-msmarco')
-dsearcher = FaissSearcher.from_prebuilt_index(
-    'msmarco-passage-tct_colbert-hnsw',
-    encoder
-)
-hsearcher = HybridSearcher(dsearcher, ssearcher)
-hits = hsearcher.search('what is a lobster roll')
+searcher = LuceneSearcher('indexes/ikat_collection_2023_01') #indexes/ikat_collection_2023_01
 
-for i in range(0, 10):
-    print(f'{i+1:2} {hits[i].docid:7} {hits[i].score:.5f}')
+def getPassagesFromSearchQuery(query):
+    hits = searcher.search(query)
+    return hits
