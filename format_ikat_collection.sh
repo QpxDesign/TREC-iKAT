05a792730f58a7d960aec412d1aaaed6feb8b455
Quinn Patwardhan <71609644+QpxDesign@users.noreply.github.com>
2023-07-28 22:10:33 -0400

    Update format_ikat_collection.sh

--- format_ikat_collection.sh
+++ format_ikat_collection.sh
@@ -3,7 +3,7 @@ read abs_path
 
 sed -i 's/ClueWeb22-ID/id/' $abs_path
 sed -i 's/Clean-Text/contents/' $abs_path
-sed -i 's/"}/"},/ $abs_path' $abs_path
+sed -i 's/"}/"},/' $abs_path
 
 echo ']' >> $abs_path
 sed -i '1s/^/[ /' $abs_path
